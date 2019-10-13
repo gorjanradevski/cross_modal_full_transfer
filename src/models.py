fix in tokenization, gradient clipping, elementwise-sum to match universal sentence encoder

src/models.py
@@ -46,7 +46,7 @@ class SentenceEncoder(nn.Module):
             param.requires_grad = finetune
 
     def forward(self, sentences: torch.Tensor):
-        embedded_sentences = torch.mean(self.bert(sentences)[0], dim=1)
+        embedded_sentences = torch.sum(self.bert(sentences)[0], dim=1)
         embedded_sentences = self.fc(embedded_sentences)
 
         return self.l2_normalize(embedded_sentences)
@@ -75,13 +75,10 @@ class ImageTextMatchingModel(nn.Module):
 
     def train(self, mode: bool = True):
         if self.finetune_image_encoder and mode:
-            print("image_encoder_in_train")
             self.image_encoder.train()
         if self.finetune_sentence_encoder and mode:
-            print("sentence_encoder_in_train")
             self.sentence_encoder.train()
         if not mode:
-            print("both in eval")
             self.image_encoder.train(mode)
             self.sentence_encoder.train(mode)
 

src/train_flickr.py
@@ -27,6 +27,7 @@ def train(
     batch_size: int,
     save_model_path: str,
     learning_rate: float,
+    clip_val: float,
     joint_space: int,
     margin: float,
     batch_hard: bool,
@@ -77,6 +78,8 @@ def train(
             loss = criterion(embedded_images, embedded_sentences)
             # backward
             loss.backward()
+            # clip the gradients
+            torch.nn.utils.clip_grad_norm_(model.parameters(), clip_val)
             # update weights
             optimizer.step()
 
@@ -119,6 +122,7 @@ def main():
         args.batch_size,
         args.save_model_path,
         args.learning_rate,
+        args.clip_val,
         args.joint_space,
         args.margin,
         args.batch_hard,
@@ -187,14 +191,13 @@ def parse_args():
         "--margin", type=float, default=0.2, help="The contrastive margin."
     )
     parser.add_argument(
-        "--gradient_clip_val", type=float, default=2.0, help="The clipping threshold."
+        "--clip_val", type=float, default=2.0, help="The clipping threshold."
     )
     parser.add_argument(
         "--batch_hard",
         action="store_true",
         help="Whether to train on the harderst negatives in a batch.",
     )
-
     parser.add_argument(
         "--finetune_image_encoder",
         action="store_true",

src/utils/datasets.py
@@ -26,9 +26,8 @@ class FlickrDataset:
                 ),
             ]
         )
-        self.tokenizer = BertTokenizer.from_pretrained(
-            "bert-base-uncased", add_special_tokens=True
-        )
+        print("adding special tokens")
+        self.tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
 
     @staticmethod
     def parse_captions_filenames(texts_path: str) -> Dict[str, List[str]]:
@@ -113,7 +112,9 @@ class FlickrDatasetTrain(FlickrDataset, TorchDataset):
         image_train_transformed = self.train_transform(image)
         image_all_transformed = self.all_transform(image_train_transformed)
 
-        caption = torch.tensor(self.tokenizer.encode(self.captions[idx]))
+        caption = torch.tensor(
+            self.tokenizer.encode(self.captions[idx], add_special_tokens=True)
+        )
 
         return image_all_transformed, caption
 
@@ -138,7 +139,9 @@ class FlickrDatasetVal(FlickrDataset, TorchDataset):
         image_val_transformed = self.val_transform(image)
         image_all_transformed = self.all_transform(image_val_transformed)
 
-        caption = torch.tensor(self.tokenizer.encode(self.captions[idx]))
+        caption = torch.tensor(
+            self.tokenizer.encode(self.captions[idx], add_special_tokens=True)
+        )
 
         return image_all_transformed, caption
 